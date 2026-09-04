e04e419b766abfddeda6754fdc910388f7abb4b8
petersinko <petersinko92@gmail.com>
2026-09-05 00:20:45 +0100

    Replace Smartsupp with Tawk.to integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,19 +37,19 @@ export default function RootLayout({
       <head>
         <script type="text/javascript" dangerouslySetInnerHTML={{
           __html: `
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '6f996554f2b901c2cc0dca358ba3602086bbaad0';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a9b47e7d862ed3449e55818/1k1n91ojf';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
           `
         }} />
       </head>
       <body className="min-h-full flex flex-col font-sans overflow-x-hidden w-full">
-        <noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
         {children}
       </body>
     </html>
